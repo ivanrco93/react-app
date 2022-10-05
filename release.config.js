85a52d3aae5621d149ccaa9a5144bb737cf947c1
Ivan Castiblanco <ivan.castiblanco@appgate.com>
2022-10-04 22:32:59 -0500

    fix: fix description

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: "master",
+    branches: "main",
     repositoryUrl: "https://github.com/ivanrco93/react-app",
     plugins: [
         '@semantic-release/commit-analyzer', 
